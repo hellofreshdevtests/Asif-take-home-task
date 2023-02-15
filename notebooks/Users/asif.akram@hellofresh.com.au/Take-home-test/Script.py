febbeaa98c09790cd5ac2643c7bb9f1517a2cdcd
AsifAkram134 <AsifAkram134>
2023-02-15 17:16:29 +0000

    g

--- notebooks/Users/asif.akram@hellofresh.com.au/Take-home-test/Script.py
+++ notebooks/Users/asif.akram@hellofresh.com.au/Take-home-test/Script.py
@@ -4,3 +4,33 @@
 
 # COMMAND ----------
 
+import os
+import pandas as pd
+os.getcwd()
+
+# COMMAND ----------
+
+import pandas as pd
+import json
+import requests
+
+url = 'https://raw.githubusercontent.com/AsifAkram134/Asif-take-home-task/Solution/input/recipes-000.json'
+response = requests.get(url)
+data = response.text.strip().split('\n')
+
+df_list = []
+
+for obj in data:
+    try:
+        parsed = json.loads(obj)
+        df_list.append(parsed)
+    except json.JSONDecodeError as e:
+        print(f"Error parsing JSON: {e}")
+
+df = pd.DataFrame(df_list)
+df
+
+
+# COMMAND ----------
+
+
